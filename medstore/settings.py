8abc43bc80222dfecfbdf85fa9a56fd48dbe7e05
rgraingerdev <121545703+rgraingerdev@users.noreply.github.com>
2023-12-05 14:17:34 +0000

    Navbar now functional CSS edit next

--- medstore/settings.py
+++ medstore/settings.py
@@ -61,14 +61,18 @@ ROOT_URLCONF = 'medstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
